8f1b225e397648d286f62d86e52af367456bfa6e
anthonyramosc <146010233+anthonyramosc@users.noreply.github.com>
2025-07-24 00:50:49 -0500

    fix

--- src/components/Main/Layout.tsx
+++ src/components/Main/Layout.tsx
@@ -508,7 +508,8 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
                                 <div onClick={handleExpoced} className="absolute cursor-pointer text-xs top-0 bg-[#464C3666] rounded-full p-2 left-20 text-white mb-4 text-center z-50">
                                     URL: Ve.site/{biosite?.slug || 'your-slug'}
                                 </div>
-                                <ShareButton/>
+                                <div className="absolute top-0">
+                                <ShareButton/></div>
                                 <button onClick={handleUpdateShareAction} disabled={buttonContent.disabled} className={`absolute text-xs top-0 rounded-lg p-2 right-20 text-white mb-4 text-center z-50 flex items-center space-x-1 transition-all duration-200 ${buttonContent.disabled ? 'bg-[#464C3666] cursor-not-allowed' : hasChanges ? 'bg-[#98C022] hover:bg-[#86A81E]' : 'bg-[#464C3666] hover:bg-[#464C36AA]'} cursor-pointer`} title={hasChanges ? 'Update preview' : 'Share link'}>
                                     {buttonContent.icon}
                                     <span>{buttonContent.text}</span>
