16e24a32043445b42e3ccef5eb0affa507f02ae7
AracnoGrpup <fgorellana@visitaecuador.com>
2025-07-28 16:35:21 -0500

    ruta

--- src/components/Main/Layout.tsx
+++ src/components/Main/Layout.tsx
@@ -278,7 +278,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
 
     const handleExpoced = () => {
         if (biosite?.slug) {
-            const url = `vesite/expoced`;
+            const url = `https://visitaecuador.com/vesite/Ve.site/${biosite?.slug || 'your-slug'}`;
             window.open(url, '_blank');
         } else {
             console.warn("No hay slug disponible para la navegación");
